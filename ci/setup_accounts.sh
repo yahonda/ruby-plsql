f92ed49625382bba8999eedbce257f02fad08030
Yasuo Honda <yasuo.honda@gmail.com>
2020-07-28 22:34:12 +0900

    Use 11g

--- ci/setup_accounts.sh
+++ ci/setup_accounts.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-${ORACLE_HOME}/bin/sqlplus system/${DATABASE_SYS_PASSWORD}@${DATABASE_NAME} <<SQL
+${ORACLE_HOME}/bin/sqlplus system/${ORACLE_SYSTEM_PASSWORD}@${DATABASE_NAME} <<SQL
 @@spec/support/unlock_and_setup_hr_user.sql
 @@spec/support/create_arunit_user.sql
 exit
